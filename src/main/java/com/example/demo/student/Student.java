f96675633547b25afd56fc36cc38a493cbafda88
NikolaiLulchev <nikolai.lulchev@gmail.com>
2022-08-24 13:06:02 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,92 @@
+package com.example.demo.student;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.Period;
+
+
+@Entity
+@Table
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    @Transient
+    private Integer age;
+
+    public Student() {
+    }
+
+    public Student(String name, String email, LocalDate dob) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+    }
+
+    public Student(Long id, String name, String email, LocalDate dob) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Student setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Student setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Student setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public Student setDob(LocalDate dob) {
+        this.dob = dob;
+        return this;
+    }
+
+    public Integer getAge() {
+        return Period.between(this.dob, LocalDate.now()).getYears();
+    }
+
+    public Student setAge(Integer age) {
+        this.age = age;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+               "id=" + id +
+               ", name='" + name + '\'' +
+               ", email='" + email + '\'' +
+               ", dob=" + dob +
+               ", age=" + age +
+               '}';
+    }
+}
